f4675b0249019fb37cf83819305e5301978fc34b
epistemex <70324091+epistemex@users.noreply.github.com>
2024-06-16 18:55:55 -0600

    Updated to modern JS and some cleanup

--- src/curve.js
+++ src/curve.js
@@ -1,8 +1,9 @@
-/*!	Curve extension for canvas 2.3.8
- *	(c) Epistemex 2013-2018
- *	www.epistemex.com
+/*!	Curve patch for canvas 2D drawing context 3.0.0
+ *	(c) Epistemex 2013-2018, 2024
  *	License: MIT
  */
+
+'use strict';
 
 /**
  * Draws a cardinal spline through given point array. Points must be arranged
@@ -25,22 +26,23 @@
  */
 CanvasRenderingContext2D.prototype.curve = CanvasRenderingContext2D.prototype.curve || function(points, tension, numOfSeg, close) {
 
-	'use strict';
-
-	if (typeof points === "undefined" || points.length < 2) return new Float32Array(0);
+	if (typeof points === "undefined" || points.length < 2) {
+		return new Float32Array(0);
+	}
 
 	// options or defaults
 	tension = typeof tension === "number" ? tension : 0.5;
 	numOfSeg = typeof numOfSeg === "number" ? numOfSeg : 25;
 
-	var pts,															// for cloning point array
-		i = 1,
-		l = points.length,
-		rPos = 0,
-		rLen = (l-2) * numOfSeg + 2 + (close ? 2 * numOfSeg: 0),
-		res = new Float32Array(rLen),
-		cache = new Float32Array((numOfSeg + 2) << 2),
-		cachePtr = 4;
+	let pts;															// for cloning point array
+	let i = 1;
+	let l = points.length;
+	let rPos = 0;
+	let cachePtr = 4;
+
+	const rLen = (l - 2) * numOfSeg + 2 + (close ? 2 * numOfSeg : 0);
+	const res = new Float32Array(rLen);
+	const cache = new Float32Array((numOfSeg + 2) << 2);
 
 	pts = points.slice(0);
 
@@ -58,18 +60,17 @@ CanvasRenderingContext2D.prototype.curve = CanvasRenderingContext2D.prototype.cu
 	// cache inner-loop calculations as they are based on t alone
 	cache[0] = 1;														// 1,0,0,0
 
-	for (; i < numOfSeg; i++) {
+	for(; i < numOfSeg; i++) {
+		const st = i / numOfSeg;
+		const st2 = st * st;
+		const st3 = st2 * st;
+		const st23 = st3 * 2;
+		const st32 = st2 * 3;
 
-		var st = i / numOfSeg,
-			st2 = st * st,
-			st3 = st2 * st,
-			st23 = st3 * 2,
-			st32 = st2 * 3;
-
-		cache[cachePtr++] =	st23 - st32 + 1;							// c1
-		cache[cachePtr++] =	st32 - st23;								// c2
-		cache[cachePtr++] =	st3 - 2 * st2 + st;							// c3
-		cache[cachePtr++] =	st3 - st2;									// c4
+		cache[cachePtr++] = st23 - st32 + 1;							// c1
+		cache[cachePtr++] = st32 - st23;								// c2
+		cache[cachePtr++] = st3 - 2 * st2 + st;							// c3
+		cache[cachePtr++] = st3 - st2;									// c4
 	}
 
 	cache[++cachePtr] = 1;												// 0,1,0,0
@@ -78,31 +79,30 @@ CanvasRenderingContext2D.prototype.curve = CanvasRenderingContext2D.prototype.cu
 	parse(pts, cache, l, tension);
 
 	if (close) {
+		//l = points.length;
 		pts = [];
 		pts.push(points[l - 4], points[l - 3],
-				 points[l - 2], points[l - 1], 							// second last and last
-				 points[0], points[1],
-				 points[2], points[3]); 								// first and second
+			points[l - 2], points[l - 1], 								// second last and last
+			points[0], points[1],
+			points[2], points[3]); 										// first and second
 		parse(pts, cache, 4, tension);
 	}
 
 	function parse(pts, cache, l, tension) {
 
-		for (var i = 2, t; i < l; i += 2) {
+		for(let i = 2; i < l; i += 2) {
+			const pt1 = pts[i];       // x1
+			const pt2 = pts[i + 1];   // y1
+			const pt3 = pts[i + 2];   // x2
+			const pt4 = pts[i + 3];   // y2
 
-			var pt1 = pts[i],
-				pt2 = pts[i+1],
-				pt3 = pts[i+2],
-				pt4 = pts[i+3],
+			const t1x = (pt3 - pts[i - 2]) * tension;  // x2-x0
+			const t1y = (pt4 - pts[i - 1]) * tension;  // y2-y0
+			const t2x = (pts[i + 4] - pt1) * tension;  // x3-x1
+			const t2y = (pts[i + 5] - pt2) * tension;  // y3-y1
+			let c = 0, c1, c2, c3, c4;
 
-				t1x = (pt3 - pts[i-2]) * tension,
-				t1y = (pt4 - pts[i-1]) * tension,
-				t2x = (pts[i+4] - pt1) * tension,
-				t2y = (pts[i+5] - pt2) * tension,
-				c = 0, c1, c2, c3, c4;
-
-			for (t = 0; t < numOfSeg; t++) {
-
+			for(let t = 0; t < numOfSeg; t++) {
 				c1 = cache[c++];
 				c2 = cache[c++];
 				c3 = cache[c++];
